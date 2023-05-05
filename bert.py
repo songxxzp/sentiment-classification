d5a60852e389f360c4c5c4372063f04210d8c34d
songxxzp <songxxzp@163.com>
2023-05-05 19:19:50 +0800

    Refactor

--- bert.py
+++ bert.py
@@ -11,27 +11,20 @@ from transformers import TrainingArguments, Trainer
 from transformers import AutoTokenizer
 from transformers import DataCollatorWithPadding
 from datasets import load_dataset
+from functools import partial
+
 import evaluate
 
 from utils import load_data
 
-model_name = "hfl/chinese-roberta-wwm-ext"
-# model_name = "bert-base-chinese"
-num_labels = 2
-train_data_file = "./Dataset/train.json"
-valid_data_file = "./Dataset/valid.json"
-test_data_file = "./Dataset/test.json"
 
-def compute_metrics(eval_pred):
+def compute_metrics(eval_pred, metric):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
 
-if __name__ == "__main__":
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
+def train_bert_classifier(model_name="hfl/chinese-roberta-wwm-ext", log_path="./log/roberta", save_path="./model/roberta", num_labels=2, batch_size=1, train_data_file = "./Dataset/train.json", valid_data_file = "./Dataset/valid.json", test_data_file = "./Dataset/test.json", device=torch.device("cpu")):
     data_train = load_dataset("json", data_files=train_data_file, field="data", split="train").shuffle()
     data_valid = load_dataset("json", data_files=valid_data_file, field="data", split="train")
     data_test = load_dataset("json", data_files=test_data_file, field="data", split="train")
@@ -58,10 +51,12 @@ if __name__ == "__main__":
     metric = evaluate.combine(["accuracy", "f1", "precision", "recall"])
 
     training_args = TrainingArguments(
-        output_dir="./roberta",
+        logging_dir=log_path,
+        output_dir=save_path,
+        save_strategy="no",
         learning_rate=2e-5,
-        per_device_train_batch_size=40,
-        per_device_eval_batch_size=40,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         num_train_epochs=4,
         do_eval=True,
         evaluation_strategy="epoch",
@@ -75,11 +70,18 @@ if __name__ == "__main__":
         eval_dataset=tokenized_data["valid"],
         tokenizer=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_metrics
+        compute_metrics=partial(compute_metrics, metric=metric)
     )
 
     trainer.train()
-    trainer.save_model("./model/roberta")
+    trainer.save_model(save_path)
 
     result = trainer.evaluate(eval_dataset=tokenized_data["test"])
     print(result)
+
+
+if __name__ == "__main__":
+    # model_name = "hfl/chinese-roberta-wwm-ext"
+    # model_name = "bert-base-chinese"
+    train_bert_classifier(model_name="hfl/chinese-roberta-wwm-ext", batch_size=32, log_path="./log/roberta", save_path="./model/roberta", device="cuda" if torch.cuda.is_available() else "cpu")
+    train_bert_classifier(model_name="bert-base-chinese", batch_size=32, log_path="./log/bert", save_path="./model/bert", device="cuda" if torch.cuda.is_available() else "cpu")
